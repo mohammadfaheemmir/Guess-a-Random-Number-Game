d63b844f5779bbc3a04ddc93a162835e4f3b5e9a
Faheem <138996989+mohammadfaheemmir@users.noreply.github.com>
2024-11-26 19:46:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,19 @@
-const hint = document.getElementById("hint");
-const noOfGuessesRef = document.getElementById("no-of-guesses");
-const guessedNumsRef = document.getElementById("guessed-nums");
-const restartButton = document.getElementById("restart");
-const game = document.getElementById("game");
-const guessInput = document.getElementById("guess");
-const checkButton = document.getElementById("check-btn");
+let generateBtn = document.getElementById("generate");
 
-let answer, noOfGuesses, guessedNumsArr;
-
-const play = () => {
-    const userGuess = guessInput.value;
-    if (userGuess < 1 || userGuess > 100 || isNaN(userGuess)) {
-        alert("Please enter a valid number between 1 and 100.");
-        return;
+function randomNum() {
+    let min = document.getElementById("min");
+    let max = document.getElementById("max");
+    let minValue = Number(min.value);
+    let maxValue = Number(max.value);
+    if (minValue > maxValue) {
+        minValue = maxValue + minValue;
+        maxValue = minValue - maxValue;
+        minValue = minValue - maxValue;
+        min.value = minValue;
+        max.value = maxValue;
     }
-    guessedNumsArr.push(userGuess);
-    noOfGuesses += 1;
-    if (userGuess != answer) {
-        if (userGuess < answer) {
-            hint.innerHTML = "Guess The Higher No!";
-        } else {
-            hint.innerHTML = "Guess The Lower No!";
-        }
-        noOfGuessesRef.innerHTML = `<span>No. Of Guesses:</span> ${noOfGuesses}`;
-        guessedNumsRef.innerHTML = `<span>Guessed Numbers are: </span>${guessedNumsArr.join(
-            ","
-        )}`;
-        hint.classList.remove("error");
-        setTimeout(() => {
-            hint.classList.add("error");
-        }, 10);
-    } else {
-        hint.innerHTML = `Congratulations!<br>The number was <span>${answer}</span>.<br>You guessed the number in <span>${noOfGuesses} </span>tries.`;
-        hint.classList.add("success");
-        game.style.display = "none";
-        restartButton.style.display = "block";
-    }
-};
-
-const init = () => {
-    console.log("Game Started");
-    answer = Math.floor(Math.random() * 100) + 1;
-    console.log(answer);
-    noOfGuesses = 0;
-    guessedNumsArr = [];
-    noOfGuessesRef.innerHTML = "No. Of Guesses: 0";
-    guessedNumsRef.innerHTML = "Guessed Numbers are: None";
-    guessInput.value = "";
-    hint.classList.remove("success", "error");
-};
-
-guessInput.addEventListener("keydown", (event) => {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        play();
-    }
-});
-
-restartButton.addEventListener("click", () => {
-    game.style.display = "grid";
-    restartButton.style.display = "none";
-    hint.innerHTML = "";
-    hint.classList.remove("success");
-    init();
-});
-
-checkButton.addEventListener("click", play);
-window.addEventListener("load", init);
+    let num = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    document.getElementById("result").innerText = num;
+}
+window.addEventListener("load", randomNum());
+generateBtn.addEventListener("click", randomNum);
